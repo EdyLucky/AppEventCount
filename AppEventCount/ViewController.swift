ea585f69d10faf7e916dc1ce24954a5f0eb670a0
Edy <elshad040@gmail.com>
2019-04-16 15:49:31 +0400

    Labels added and codes added for each label to be update based on the methods to be called from AppDelegate

--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var didFinishLaunchLabel: UILabel!
+    @IBOutlet weak var willResignActiveLabel: UILabel!
+    @IBOutlet weak var didEnterBackground: UILabel!
+    @IBOutlet weak var willEnterForeground: UILabel!
+    @IBOutlet weak var didBecomeActive: UILabel!
+    @IBOutlet weak var willTerimnate: UILabel!
+    
+    var launchCount = 0
+    var resignCount = 0
+    var enterBackgrounCount = 0
+    var willEnterCount = 0
+    var didBecomeActiveCount = 0
+    var willTerminateCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateView() {
+        didFinishLaunchLabel.text = "The App has launched \(launchCount) times"
+        willResignActiveLabel.text = "The App has resigned \(resignCount) times"
+        didEnterBackground.text = "The App has entered Background \(enterBackgrounCount) times"
+        willEnterForeground.text = "The App has entered Foreground \(willEnterCount) times"
+        didBecomeActive.text = "The App has became active \(didBecomeActiveCount) times"
+        willTerimnate.text = "The App will terminate \(willTerminateCount) times"
     }
 
 
